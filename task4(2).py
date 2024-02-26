97aea0aed5f9d1bb4d6beee35df5b0101054c114
nstvslna <160972387+nstvslna@users.noreply.github.com>
2024-02-26 16:10:48 +0200

    second commit

--- task4(2).py
+++ task4(2).py
@@ -3,9 +3,13 @@ a = int(input())
 b = int(input())
 c = int(input())
 
-max_num = a * (a > b and a > c) + b * (b > a and b > c) + c * (c > a and c > b)
-min_num = a * (a < b and a < c) + b * (b < a and b < c) + c * (c < a and c < b)
-mid_num = a * (min_num < a < max_num) + b * (min_num < b < max_num) + c * (min_num < c < max_num)
+max_num = (a >= b) * a + (b > a) * b
+max_num = (max_num >= c) * max_num + (c > max_num) * c
+
+min_num = (a <= b) * a + (b < a) * b
+min_num = (min_num <= c) * min_num + (c < min_num) * c
+
+mid_num = (a + b + c) - max_num - min_num
 
 print("Результат:")
 print(max_num)
