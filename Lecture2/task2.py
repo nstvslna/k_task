30b9874760de84fd35209d72d7bf4675d47f0085
nstvslna <160972387+nstvslna@users.noreply.github.com>
2024-03-05 09:30:09 +0200

    2 commit

--- Lecture2/task2.py
+++ Lecture2/task2.py
@@ -1,4 +1,3 @@
 players = ['Ashleigh Barty', 'Simona Halep', 'Naomi Osaka', 'Karolina Pliskova', 'Elina Svitolina']
-players[0] = players[-1]
-players[-1] = players[0]
+players[0], players[-1] = players[-1], players[0]
 print("Список гравців після обміну:", players)
