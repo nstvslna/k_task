d46da5d978325abf6ffdbc77f063d598ffd5709a
nstvslna <160972387+nstvslna@users.noreply.github.com>
2024-03-05 09:21:22 +0200

    first commit

---
+++ Lecture2/task3.py
@@ -0,0 +1,18 @@
+quote = ("The reasonable man adapts himself to the world; the unreasonable one persists in tryingto adapt the world to "
+         "himself.")
+
+# Розділити цитату на список слів
+words = quote.split()
+
+# Знаходимо індекс слів "reasonable" і "unreasonable"
+index_reasonable = words.index("reasonable")
+index_unreasonable = words.index("unreasonable")
+
+# Змінюємо позиції
+words[index_reasonable] = words[index_unreasonable]
+words[index_unreasonable] = words[index_reasonable]
+
+# Об'єднуємо список в рядок
+new_quote = " ".join(words)
+
+print(new_quote)
